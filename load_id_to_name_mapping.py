8ed999d1a2c7b1a2428b6497c0f4137856cc836a
JackJosephWright <70408833+JackJosephWright@users.noreply.github.com>
2024-12-16 14:33:09 -0500

    changed refs for load_id_to_wr_mappings

--- load_id_to_name_mapping.py
+++ load_id_to_name_mapping.py
@@ -7,6 +7,6 @@ def load_id_to_name_mapping():
     Returns:
         dict: A dictionary mapping card IDs to GP WR.
     """
-    cards = pd.read_csv(r'C:\Users\Jack Wright\Documents\GitHub\sevLandsPublicData\data\cards_data\cards.csv')
+    cards = pd.read_csv("./data/cards.csv")
     name_mapping = cards[['id', 'name']]
     return name_mapping.set_index('id')['name'].to_dict()
